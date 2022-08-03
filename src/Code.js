59df695781d17288f890a7374d35250914d29129
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-03 23:46:57 +0300

    edit code function

--- src/Code.js
+++ src/Code.js
@@ -1,25 +1,12 @@
 import React, { useState } from "react";
 
-const Code = () => {
-  const string = [
-    `// Creating some variables`,
-    `var v1 = {name: "John", age: 18};`,
-    `var v2 = ["red", "green", "blue", "yellow"];`,
-    `var v3 = [1, 2, 3, 4, 5];`,
-    `var v4 = null;`,
-    "",
-    `// Testing the variables data type`,
-    `typeof(v1); // Returns: "object"`,
-    `typeof(v2); // Returns: "object"`,
-    `typeof(v3); // Returns: "object"`,
-    `typeof(v3); // Returns: "object"`,
-    "",
-    `// Testing if the variable is an array`,
-    ` Array.isArray(v1);  // Returns: false`,
-    `Array.isArray(v2);  // Returns: true`,
-    `Array.isArray(v3);  // Returns: true`,
-    `Array.isArray(v4);  // Returns: false`,
-  ];
+const codeFunction = (codeData) => {
+  // Перенос строки
+  // Отступы слева
+  // Номерация рядов
+  // Цвет кода для типов
+  //// Коментарии
+  // Копирование кода
 };
 
 export default Code;
